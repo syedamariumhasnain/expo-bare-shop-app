28a4566a3ec04ea625febe0aba3db0b2dbb76184
syedamariumhasnain <syedamarium27@gmail.com>
2021-01-08 01:07:12 +0500

    Initail Setup + Fonts Setup + Redux / model / dummy-data

--- App.js
+++ App.js
@@ -1,21 +1,78 @@
+import React, { useState } from 'react';
+import * as Font from "expo-font";
+import AppLoading from 'expo-app-loading'
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
 
 export default function App() {
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+// import React, { useState } from "react";
+// import * as Font from "expo-font";
+// import { AppLoading } from "expo";
+// import { enableScreens } from "react-native-screens";
+// import { createStore, combineReducers } from "redux";
+// import { Provider } from "react-redux";
+
+// import MealsNavigator from "./navigation/MealsNavigator";
+// import mealsReducer from "./store/reducers/meals";
+
+// enableScreens();
+
+// const rootReducer = combineReducers({
+//   meals: mealsReducer,
+// });
+
+// const store = createStore(rootReducer);
+
+// const fetchFonts = () => {
+//   return Font.loadAsync({
+//     "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+//     "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+//   });
+// };
+
+// export default function App() {
+//   const [dataLoaded, setDataLoaded] = useState(false);
+
+//   if (!dataLoaded) {
+//     return (
+//       <AppLoading
+//         startAsync={fetchFonts}
+//         onFinish={() => setDataLoaded(true)}
+//         onError={(err) => console.log(err)}
+//       />
+//     );
+//   }
+
+//   return (
+//     <Provider store={store}>
+//       <MealsNavigator />
+//     </Provider>
+//   );
+// }
