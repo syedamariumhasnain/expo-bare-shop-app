5ca380bad74a6baa3eaefb5a7866fcd3b81a14e3
syedamariumhasnain <syedamarium27@gmail.com>
2021-02-24 01:05:41 +0500

    storing Orders on firebase

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -3,8 +3,37 @@ import CartItem from "../../models/cart-item";
 export const ADD_ORDER = "ADD_ORDER";
 
 export const addOrder = (cartItems, totalAmount) => {
-  return {
-    type: ADD_ORDER,
-    orderData: { items: cartItems, amount: totalAmount }
+  const date = new Date();
+  return async (dispatch) => {
+    const response = await fetch(
+      "https://shop-app-3caa2-default-rtdb.firebaseio.com/orders/u1.json",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          cartItems,
+          totalAmount,
+          date: date.toISOString(),
+        }),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Something went wrong!");
+    }
+
+    const resData = await response.json();
+
+    dispatch({
+      type: ADD_ORDER,
+      orderData: {
+        id: resData.name,
+        items: cartItems,
+        amount: totalAmount,
+        date: date,
+      },
+    });
   };
-}
+};
